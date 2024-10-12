cd8be3ce5606d2aab1e8899afea752a9409c1b51
rouk5555 <153026365+rouk5555@users.noreply.github.com>
2024-10-11 23:43:32 -0400

    Add files via upload

---
+++ Web-Scraper.ps1
@@ -0,0 +1,68 @@
+﻿function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://localhost/Courses.html
+
+    # Get all tr elements
+    $trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+
+    # Empty array
+    $FullTable = @()
+    for ($i = 1; $i -lt $trs.length; $i++) {  
+
+        # Get every td element
+        $tds = $trs[$i].getElementsByTagName("td")
+
+        # Separate start time and end time
+        $Times = $tds[5].innerText.Split("-")
+
+        # Create custom object
+        $FullTable += [PSCustomObject]@{
+            "Class Code" = $tds[0].innerText;
+            "Title"      = $tds[1].innerText;
+            "Days"       = $tds[4].innerText;
+            "Time Start" = $Times[0]
+            "Time End"   = $Times[1]
+            "Instructor" = $tds[6].innerText;
+            "Location"   = $tds[9].innerText;
+        }
+    }
+
+    # Call daysTranslator
+    $FullTable = daysTranslator($FullTable)
+
+
+    return $FullTable
+
+}
+
+# Function to translate days
+function daysTranslator($FullTable) {
+    # Go over every record in table
+    for ($i = 0; $i -lt $FullTable.length; $i++) {
+
+        # Empty array
+        $Days = @()
+
+        # M = Monday
+        if ($FullTable[$i].Days -ilike "M*") { $Days += "Monday" }
+
+        # T followed by T,W, or F = Tuesday
+        if ($FullTable[$i].Days -ilike "*T[TWF]*") { $Days += "Tuesday" }
+
+        # Only T = Tuesday
+        ElseIf ($FullTable[$i].Days -ilike "T") { $Days += "Tuesday" }
+
+        # W = Wednesday
+        if ($FullTable[$i].Days -ilike "*W*") { $Days += "Wednesday" }
+
+        # TH = Thursday
+        if ($FullTable[$i].Days -ilike "*TH*") { $Days += "Thursday" }
+
+        # F = Friday
+        if ($FullTable[$i].Days -ilike "*F") { $Days += "Friday" }
+
+        # Make switch
+        $FullTable[$i].Days = $Days
+    }
+
+    return $FullTable
+}
---
+++ main.ps1
@@ -0,0 +1,25 @@
+﻿. (Join-Path $PSScriptRoot Web-Scraper.ps1)
+
+clear
+
+$FullTable = gatherClasses
+
+# Various queries
+$FullTable | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" | `
+            Where-Object { $_.Instructor -eq "Furkan Paligu" }
+
+$FullTable | Where-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -icontains "Monday") } | `
+            Sort-Object "Time Start" | `
+            Format-Table "Time Start", "Time End", "Class Code"
+ 
+$ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                              ($_."Class Code" -ilike "NET*") -or `
+                                              ($_."Class Code" -ilike "SEC*") -or `
+                                              ($_."Class Code" -ilike "FOR*") -or `
+                                              ($_."Class Code" -ilike "CSI*") -or `
+                                              ($_."Class Code" -ilike "DAT*") } `
+                             | Select-Object "Instructor" `
+                             | Sort-Object "Instructor" -Unique
+
+$FullTable | where { $_.Instructor -in $ITSInstructors.Instructor } `
+           | Group-Object "Instructor" | Select-Object Count,Name | Sort-Object Count -Descending
